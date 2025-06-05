5bf9183fc9c55ad1d67648a3f9073fedef33f2a6
nicetomeet <dhika.mahardp@gmail.com>
2025-06-05 17:46:36 +0700

    redid the save to json (unfinished)

--- modules/notes_json/scripts/NoteWritingJson.cs
+++ modules/notes_json/scripts/NoteWritingJson.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 using System.Text.Json;
 
@@ -16,7 +17,6 @@ public partial class NoteWritingJson : VBoxContainer
         closeButton = GetNode<Button>("%CloseButton");
         noteArea = GetNode<TextEdit>("%NoteWriting");
         noteTitle = GetNode<Label>("%NoteTitle");
-        noteUID = GetNode<Label>("%NoteUID");
 
         SaveToJson();
     }
@@ -27,67 +27,21 @@ public partial class NoteWritingJson : VBoxContainer
     {
         string jsonPath = "res://modules/notes_json/data/notes_data.json";
 
+        // Godot Dictionary, do not confuse with C# Dictionary
+        Dictionary<string, Dictionary<string, string>> note =
+            new Dictionary<string, Dictionary<string, string>>();
+        Dictionary<string, string> data = new Dictionary<string, string>();
 
-        Godot.Collections.Array<Godot.Collections.Dictionary<string, string>> noteData =
-            new Godot.Collections.Array<Godot.Collections.Dictionary<string, string>>();
+        data.Add("note_title", "How to strike it rich");
+        data.Add("note_desc", "Legit 100%!!");
+        data.Add("note_content", "Who knows?");
 
-        noteData.Add(new Godot.Collections.Dictionary<string, string>());
-        noteData[0].Add("note_id", "001");
-        noteData[0].Add("note_title", "note_title_01");
-        noteData[0].Add("note_content", "note_content_01");
+        note.Add("0002", data);
 
-
-        string noteDataToString = JsonSerializer.Serialize(noteData);
-        GD.Print(noteDataToString.GetType());
-
-        if (!FileAccess.FileExists(jsonPath))
-        {
-            FileAccess fileObj = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Write);
-            fileObj.StoreLine(noteDataToString);
-            GD.Print("File doesn't exist, creating one...");
-        }
-        else
-        {
-            // Read existing Json
-            FileAccess fileObj = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Read);
-            string fileObjStringify = fileObj.GetLine();
-            Variant parsedFileObjStringify = Json.ParseString(fileObjStringify);
-
-            Godot.Collections.Array<Godot.Collections.Dictionary<string, string>> parsedFileObjStringifyToArray =
-                parsedFileObjStringify.AsGodotArray<Godot.Collections.Dictionary<string, string>>();
-
-            GD.Print("File exist...");
-            GD.Print(fileObjStringify.GetType());
-            GD.Print(parsedFileObjStringifyToArray.Count);
-        }
+        FileAccess noteFile = FileAccess.Open(jsonPath, FileAccess.ModeFlags.WriteRead);
+        noteFile.StoreLine(Json.Stringify(note));
     }
 
-
-    public string GenerateUID()
-    {
-        // Generate uid like: "note00001, note00002, etc"
-        // Make sure there's no duplicate from previous created notes
-        string jsonPath = "res://modules/notes_json/data/notes_data.json";
-        string noteUID = "note";
-
-        if (!FileAccess.FileExists(jsonPath))
-        {
-            noteUID = noteUID + 1;
-        }
-        else
-        {
-            FileAccess fileObj = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Read);
-            string fileObjStringify = fileObj.GetLine();
-            Variant parsedFileObjStringify = Json.ParseString(fileObjStringify);
-
-            Godot.Collections.Array<Godot.Collections.Dictionary<string, string>> parsedFileObjToArray =
-                parsedFileObjStringify.AsGodotArray<Godot.Collections.Dictionary<string, string>>();
-
-            int numOfArray = parsedFileObjToArray.Count;
-            noteUID = numOfArray + 1;
-        }
-        return noteUID;
-    }
 
 
     // SIGNAL AREA
