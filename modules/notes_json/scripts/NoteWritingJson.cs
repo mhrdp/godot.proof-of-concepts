0c21aecfa49b20b6b6f807884aa20864b3495e17
nicetomeet <dhika.mahardp@gmail.com>
2025-06-09 18:21:52 +0700

    read data from json

--- modules/notes_json/scripts/NoteWritingJson.cs
+++ modules/notes_json/scripts/NoteWritingJson.cs
@@ -1,7 +1,6 @@
 using Godot;
 using Godot.Collections;
 using System;
-using System.Text.Json;
 
 public partial class NoteWritingJson : VBoxContainer
 {
@@ -38,8 +37,26 @@ public partial class NoteWritingJson : VBoxContainer
 
         note.Add("0002", data);
 
-        FileAccess noteFile = FileAccess.Open(jsonPath, FileAccess.ModeFlags.WriteRead);
-        noteFile.StoreLine(Json.Stringify(note));
+        if (!FileAccess.FileExists(jsonPath))
+        {
+            FileAccess createNoteFile = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Write);
+            createNoteFile.StoreLine(Json.Stringify(note));
+        }
+        else
+        {
+            Json noteFile = new Json();
+            FileAccess readNoteFile = FileAccess.Open(jsonPath, FileAccess.ModeFlags.Read);
+            Error parsedNoteFile = noteFile.Parse(readNoteFile.GetAsText());
+
+            if (parsedNoteFile == Error.Ok)
+            {
+                Dictionary<string, Dictionary<string, string>> noteFileDict =
+                    noteFile.Data.AsGodotDictionary<string, Dictionary<string, string>>();
+
+            }
+            readNoteFile.Close();
+
+        }
     }
 
 
