f1df1114fafc08ad17f9f4c199ca29520ae1088f
nicetomeet <dhika.mahardp@gmail.com>
2025-06-15 15:17:00 +0700

    json stuff

--- modules/notes_json/scripts/NoteWritingJson.cs
+++ modules/notes_json/scripts/NoteWritingJson.cs
@@ -32,14 +32,14 @@ public partial class NoteWritingJson : GDGeneric.VBoxContainer
     public class Note
     {
         public string noteId { get; set; }
-        public GDCollections.Dictionary<string, string> noteContent { get; set; }
+        public GDCollections.Dictionary<string, string> noteData { get; set; }
 
         public GDCollections.Dictionary<string, GDGeneric.Variant> ToVariantDictionary()
         {
             var dict = new GDCollections.Dictionary<string, GDGeneric.Variant>
             {
                 {"note_id", noteId},
-                {"note_content", noteContent},
+                {"note_data", noteData},
             };
             return dict;
         }
@@ -58,24 +58,37 @@ public partial class NoteWritingJson : GDGeneric.VBoxContainer
         data.Add("note_desc", "Legit 100%!!");
         data.Add("note_content", "Who knows?");
 
-        notes.noteId = "0002";
-        notes.noteContent = data;
+        notes.noteId = "0";
+        notes.noteData = data;
 
         noteData.Add(notes.ToVariantDictionary());
 
-        string jsonString2 = GDGeneric.Json.Stringify(noteData);
+        string jsonString = GDGeneric.Json.Stringify(noteData);
 
         if (!GDGeneric.FileAccess.FileExists(jsonPath))
         {
             edit = false;
             GDGeneric.FileAccess createNoteFile = GDGeneric.FileAccess.Open(jsonPath, GDGeneric.FileAccess.ModeFlags.Write);
-            createNoteFile.StoreLine(jsonString2);
+            createNoteFile.StoreLine(jsonString);
         }
         else
         {
             GDGeneric.Json noteFile = new GDGeneric.Json();
             GDGeneric.FileAccess readNoteFile = GDGeneric.FileAccess.Open(jsonPath, GDGeneric.FileAccess.ModeFlags.Read);
             GDGeneric.Error parsedNoteFile = noteFile.Parse(readNoteFile.GetAsText());
+
+            if (parsedNoteFile == GDGeneric.Error.Ok)
+            {
+                GDGeneric.Variant jsonData = noteFile.Data;
+                GDCollections.Array<GDCollections.Dictionary<string, GDGeneric.Variant>> jsonDataDict =
+                    jsonData.AsGodotArray<GDCollections.Dictionary<string, GDGeneric.Variant>>();
+
+                int generateNoteId = jsonDataDict.Count + 1;
+            }
+            else
+            {
+                GDGeneric.GD.Print(noteFile.GetErrorMessage());
+            }
 
             readNoteFile.Close();
 
